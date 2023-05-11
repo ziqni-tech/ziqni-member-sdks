3d2b6127e6832fc92a0edc388a890f03a481e2a2
vossie <carel.vosloo@bronzegate.com>
2023-05-11 11:08:24 +0100

    Adding swift

--- swift5/OpenAPIClient/Classes/OpenAPIs/StompWebSocketClient.swift
+++ swift5/OpenAPIClient/Classes/OpenAPIs/StompWebSocketClient.swift
@@ -1,63 +1,96 @@
-import StompClientLib
-import Foundation
+import SwiftStomp
 
-// This implementation uses the StompClientLib library, which is available through CocoaPods or Carthage.
-// The StompWebSocketClient class provides methods for connecting to a STOMP server, subscribing to a destination, sending a message, and disconnecting from the server.
-//
+// In this example, we use the SwiftStomp library to handle the STOMP WebSocket communication.
+The StompWebSocketClient class creates an instance of StompClient with the provided URL,
+and provides methods for connecting, subscribing, sending messages, and disconnecting.
+// ------------
 // To use this class, create an instance with the URL of the STOMP WebSocket endpoint:
-//  let url = URL(string: "ws://localhost:8080/stomp")!
-//  let client = StompWebSocketClient(url: url)
-//
+// ------------
+// let url = URL(string: "ws://localhost:8080/stomp")!
+// let client = StompWebSocketClient(url: url)
+// ------------
 // Then, call the connect method to authenticate and connect to the server:
-//    client.connect(username: "username", password: "password") {
-//        print("Connected to STOMP server")
-//    }
-// You can then subscribe to a destination and receive messages with the subscribe method:
-//
-// client.subscribe(to: "/topic/chat") { message in
-//     print("Received message: \(message)")
+// ------------
+// client.connect(username: "username", password: "password") {
+//     print("Connected to STOMP server")
 // }
-// To send a message to a destination, use the send method:
-//
-// client.send(to: "/topic/chat", body: "Hello, world!")
-// Finally, to disconnect from the server, call the disconnect method:
-//
-// client.disconnect()
-// Note that this is a basic example and does not include error handling or other advanced features. For more information on using the StompClientLib library, refer to the library's documentation.
+
 class StompWebSocketClient {
-    public static var basePath = URL("ws://member-api.ziqni.com/ws")
-    public static var rpcTopic = "/user/queue/rpc-results"
-    public static var callbacksTopic = "/user/queue/callbacks"
-
-    private let client: StompClientLib
-
-    init() {
-        client = StompClientLib()
-        client.openSocketWithURL(basePath)
-    }
+    private let stompClient: StompClient
 
     init(url: URL) {
-        client = StompClientLib()
-        client.openSocketWithURL(url)
+        stompClient = StompClient(url: url)
+
+        // Optional: configure the StompClient with custom options
+        stompClient.configuration = StompClientConfiguration(
+            shouldConnectAutomatically: true,
+            shouldAutoDisconnectOnWebSocketClose: true,
+            reconnectInterval: 5
+        )
+
+        // Optional: set the delegate to receive StompClient events
+        stompClient.delegate = self
     }
 
     func connect(username: String?, password: String?, completionHandler: @escaping (() -> Void)) {
-        client.connect(username: username, passcode: password) { _, _ in
+        // Perform STOMP-specific connection logic using the SwiftStomp library
+        // For example, sending STOMP CONNECT frame with username and password
+
+        let headers = [
+            StompHeader(key: .login, value: username ?? ""),
+            StompHeader(key: .passcode, value: password ?? "")
+        ]
+
+        stompClient.connect(withHeaders: headers) { _ in
             completionHandler()
         }
     }
 
     func subscribe(to destination: String, completionHandler: @escaping ((String?) -> Void)) {
-        client.subscribe(destination: destination) { _, message in
-            completionHandler(message)
+        // Perform STOMP-specific subscription logic using the SwiftStomp library
+        // For example, sending STOMP SUBSCRIBE frame to the given destination
+
+        let headers = [StompHeader(key: .destination, value: destination)]
+
+        stompClient.subscribe(to: headers) { result in
+            switch result {
+            case .success(let subscription):
+                completionHandler("Subscribed with ID \(subscription.id)")
+            case .failure(let error):
+                completionHandler("Failed to subscribe: \(error.localizedDescription)")
+            }
         }
     }
 
     func send(to destination: String, body: String) {
-        client.sendJSONForDict(dict: ["destination": destination, "body": body])
+        // Perform STOMP-specific message sending logic using the SwiftStomp library
+        // For example, sending STOMP SEND frame with the given destination and body
+
+        let headers = [StompHeader(key: .destination, value: destination)]
+        let message = StompMessage(headers: headers, body: body)
+
+        stompClient.send(message)
     }
 
     func disconnect() {
-        client.disconnect()
+        stompClient.disconnect()
     }
+}
+
+// MARK: - StompClientDelegate Methods
+
+extension StompWebSocketClient: StompClientDelegate {
+    func stompClientDidDisconnect(client: StompClient, error: Error?) {
+        print("Disconnected from STOMP server")
+    }
+
+    func stompClientDidConnect(client: StompClient) {
+        print("Connected to STOMP server")
+    }
+
+    func stompClientDidReceiveMessage(client: StompClient, message: StompMessage) {
+        print("Received message: \(message.body)")
+    }
+
+    // Implement other StompClientDelegate methods as needed
 }
