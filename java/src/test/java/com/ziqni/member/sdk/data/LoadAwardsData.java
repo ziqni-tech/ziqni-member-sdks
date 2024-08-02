f6fa84e2358392a188754e28c2dd3884e9d65a8b
GivenTP-ziqni <given@ziqni.com>
2024-08-02 14:53:22 +0200

    added test to check if claimed Awards Response returns claimed status code after an award has been claimed

--- java/src/test/java/com/ziqni/member/sdk/data/LoadAwardsData.java
+++ java/src/test/java/com/ziqni/member/sdk/data/LoadAwardsData.java
@@ -3,14 +3,25 @@ package com.ziqni.member.sdk.data;
 import com.ziqni.member.sdk.model.AwardFilter;
 import com.ziqni.member.sdk.model.AwardRequest;
 import com.ziqni.member.sdk.model.ClaimAwardRequest;
+import com.ziqni.member.sdk.model.NumberRange;
 import tests.utils.CompleteableFutureTestWrapper;
 
 import java.util.List;
 
 public class LoadAwardsData implements CompleteableFutureTestWrapper {
 
-    public AwardRequest getRequest(String memberRefId) {
-        return new AwardRequest().awardFilter(new AwardFilter());
+    public AwardRequest getRequest(String awardId) {
+        final var numRange = new NumberRange();
+        numRange.setLessThan(16L);
+        numRange.setMoreThan(14L);
+
+        return new AwardRequest()
+                .awardFilter(new AwardFilter()
+                                .statusCode(numRange)
+                                .ids(List.of(awardId))
+//                        .lifecycleStatus(AwardFilter.LifecycleStatusEnum.ARCHIVED)
+                )
+                .languageKey("en");
 //                .memberRefId(memberRefId);
 
     }
